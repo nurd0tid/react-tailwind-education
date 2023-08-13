98b00ed62503c15df952828dee4f89efa96e8aa5
nurd0tid <mnur29054@gmail.com>
2023-08-13 17:26:46 +0700

    update

--- src/components/container/Course/Course.jsx
+++ src/components/container/Course/Course.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { AiOutlineUser, AiOutlineStar } from 'react-icons/ai'
 
 const Course = ({image, category, title, participants, rating, price}) => {
   return (
     <div className='p-2 shadow-lg min-w-[15rem] bg-white rounded-md'>
       <img src={image} alt=""/>
-      <div>
+      <div className='mt-2 text-xs text-Teal'>
         {category}
       </div>
-      <div>
+      <div className='text-sm mt-2 font-bold'>
         {title}
       </div>
-      <div>
-        {participants}
+      <div className='flex items-center justify-between mt-2'>
+        <div className='flex items-center gap-2'>
+          <div className='bg-Solitude p-1 rounded-full'>
+            <AiOutlineUser className='text-Teal'/>
+          </div>
+          <div className='text-sm font-bold'>
+            {participants}
+          </div>
+        </div>
+        <div className='flex items-center gap-2'>
+          <div className='bg-Solitude p-1 rounded-full'>
+            <AiOutlineStar className='text-yellow'/>
+          </div>
+          <div className='text-sm font-bold'>{rating}</div>
+        </div>
+        <div className='text-sm font-bold'>
+          ${price}
+        </div>
       </div>
     </div>
   )
