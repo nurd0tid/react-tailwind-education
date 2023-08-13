c60c76db7b76f68ac8187ad13b8fa95351fcf5b4
nurd0tid <mnur29054@gmail.com>
2023-08-13 17:33:36 +0700

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className={`${active ? "shadow-lg bg-Solitude" : ""} fixed w-full top-0 left-0 z-20`}>
       <div>
-        <div className='container py-4 mx-auto flex items-center justify-between px-2'>
+        <div className={`${active ? "py-2 transition-all duration-300" : ""} container py-4 mx-auto flex items-center justify-between px-2`}>
           <div className='flex items-center gap-4'>
             <HiMenuAlt1 className='text-3xl sm:hidden cursor-pointer' onClick={() => setToggle(true)}/>
             <div className='text-xl text-Teal uppercase tracking-wide font-bold'>
