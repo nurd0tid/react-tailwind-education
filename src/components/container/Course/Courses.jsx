3bdad56630238a5f735ea560fca66e004d87cb2a
nurd0tid <mnur29054@gmail.com>
2023-08-13 16:57:07 +0700

    update

--- src/components/container/Course/Courses.jsx
+++ src/components/container/Course/Courses.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { categories, courses } from '../../../Data'
+import Categories from './Categories'
 
 const Courses = () => {
   return (
-    <div className='section' id='courses'>Course</div>
+    <div className='section' id='courses'>
+      <div className='text-center'>
+        <div className='sm:text-3xl text-2xl font-bold mb-5'>
+          Our Top <span className='text-Teal'>Categories</span>
+        </div>
+        <p className='text-sm text-gray leading-7 max-w-[700px] mx-auto'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, officiis voluptate fugit eaque amet optio laudantium cupiditate nisi laboriosam deserunt officia fugiat. Ad ratione dolor sint sed, distinctio facere delectus?</p>
+      </div>
+      <div className='grid md:grid-cols-4 sm:grid-cols-2 mt-12 gap-8'>
+        {categories.map(category => {
+          return <Categories key={category.id} {...category}/>
+        })}
+      </div>
+    </div>
   )
 }
 
